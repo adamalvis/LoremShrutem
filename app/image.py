2f7001748cb12e888d5a070ad7aeacc1b4ba8b87
Adam Alvis <aalvis@chappellroberts.com>
2018-03-15 08:50:00 -0400

    added new dwights, removed get_target_size method from PlaceholderImage

--- app/image.py
+++ app/image.py
@@ -14,21 +14,14 @@ class PlaceholderImage:
     new_file = '{}.jpg'.format(uuid4())
     new_file_path = os.path.join(CROPPED_IMAGE_DIR, new_file)
 
-    self.resizecrop(self.image_file, new_file_path, width, height)
+    self._resizecrop(self.image_file, new_file_path, width, height)
 
     self.cropped_image = new_file_path
 
-  def resizecrop(self, src, out, width, height):
+  def _resizecrop(self, src, out, width, height):
     img = Image.open(src)
     img = ImageOps.fit(img, (width, height), Image.ANTIALIAS, 0, (0.5, 0.5))
     img.save(out)
 
   def get_file(self):
     return self.cropped_image if self.cropped_image else self.image_file
-
-  def get_target_size(self, image, new_width, new_height) -> tuple:
-    """Returns a tuple of (width,height)"""
-    width, height = image.size
-    if width > height:
-      return (new_width, int((width / height) * new_height))
-    return (new_height, int((height / width) * new_width))
--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from .image import PlaceholderImage
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
   return render_template('index.html')
